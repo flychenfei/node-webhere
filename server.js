adc40342db8a4799eb741be50afc682cf2ae5b5c
jeremychone <jeremy.chone@gmail.com>
2016-03-16 13:10:41 -0700

    - fixed the hapi 8.x directory handler issue (not provided by default)

--- server.js
+++ server.js
@@ -3,40 +3,42 @@
 'use strict';
 
 var Hapi = require( 'hapi' );
+var Inert = require('inert');
 var exec = require( 'child_process' ).exec;
 
 var isWin = /^win/.test( process.platform );
 
 var port = process.argv[ 2 ];
 if( port === undefined ){
-    port = 8888;
+	port = 8888;
 }
 var server = new Hapi.Server();
 server.connection({ port: port, routes: { cors: true }});
+server.register(Inert, function () {});
 
 var root = process.cwd() + '/';
 
 server.route({
-    method: '*',
-    path: '/{path*}',
-    handler: {
-        directory: {
-            path: function( request ){
-                console.log( ' > ' + new Date().getTime() + ' ' + request.method.toUpperCase() + ' ' + request.path );
-                return root;
-            },
-            listing: true,
-            index: [ 'index.html', 'default.html' ]
-        }
-    }
+	method: '*',
+	path: '/{path*}',
+	handler: {
+		directory: {
+			path: function( request ){
+				console.log( ' > ' + new Date().getTime() + ' ' + request.method.toUpperCase() + ' ' + request.path );
+				return root;
+			},
+			listing: true,
+			index: [ 'index.html', 'default.html' ]
+		}
+	}
 });
 
 server.start( function(){
-    console.log( 'Server address: http://localhost:%d', port );
-    console.log( 'Server directory: %s', root );
-    if( isWin ){
-        exec( 'start http://localhost:' + port, function( error, stdout, stderr ){});
-    }else{
-        exec( 'open http://localhost:' + port, function( error, stdout, stderr ){});
-    }
+	console.log( 'Server address: http://localhost:%d', port );
+	console.log( 'Server directory: %s', root );
+	if( isWin ){
+		exec( 'start http://localhost:' + port, function( error, stdout, stderr ){});
+	}else{
+		exec( 'open http://localhost:' + port, function( error, stdout, stderr ){});
+	}
 });

3a9272eed11da7be2cb781c184b710cde4162c74
jeremychone <jeremy.chone@gmail.com>
2016-03-16 12:51:33 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Inspired from localhapi 
+Strongly inspired: https://github.com/diverted247/localhapi
+From Ted Patrick <ted@tedpatrick.com> (http://tedpatrick.com/)
+
+# Changelog from localhapi 
+- Updated hapi to 13.2.1
+- Add header "Access-Control-Allow-Origin" "*" to all request. 
+
+# Install
+npm install -g webhere
+
+# Use
+Start hapijs webserver on port 9000 and start browser when complete
+```
+cd ~/Dev/amazingwebapp
+_wh 9000
+```
+
+Start hapijs webserver on port 8888(default) and start browser when complete
+```
+cd ~/Dev/superwebsite
+_wh
+```
+
+# Thanks fot 
+Cheers!
+
+Ted :)
---
+++ server.js
@@ -0,0 +1,42 @@
+#! /usr/bin/env node
+
+'use strict';
+
+var Hapi = require( 'hapi' );
+var exec = require( 'child_process' ).exec;
+
+var isWin = /^win/.test( process.platform );
+
+var port = process.argv[ 2 ];
+if( port === undefined ){
+    port = 8888;
+}
+var server = new Hapi.Server();
+server.connection({ port: port, routes: { cors: true }});
+
+var root = process.cwd() + '/';
+
+server.route({
+    method: '*',
+    path: '/{path*}',
+    handler: {
+        directory: {
+            path: function( request ){
+                console.log( ' > ' + new Date().getTime() + ' ' + request.method.toUpperCase() + ' ' + request.path );
+                return root;
+            },
+            listing: true,
+            index: [ 'index.html', 'default.html' ]
+        }
+    }
+});
+
+server.start( function(){
+    console.log( 'Server address: http://localhost:%d', port );
+    console.log( 'Server directory: %s', root );
+    if( isWin ){
+        exec( 'start http://localhost:' + port, function( error, stdout, stderr ){});
+    }else{
+        exec( 'open http://localhost:' + port, function( error, stdout, stderr ){});
+    }
+});
